85a1cf7fd26a73270c33e95965904e4bc7d5b09b
Thomas Peter <98786221+Sergei-007@users.noreply.github.com>
2022-08-09 12:37:57 -0500

    Initial commit

---
+++ src/BaseStats.js
@@ -0,0 +1,41 @@
+import "./BaseStats.css";
+
+const BaseStats = ({
+  clicker,
+  stats: { hp, attack, defense, speed },
+  moves
+}) => {
+  return (
+    <div className="stats">
+      <h1>BaseStats</h1>
+      <div className="base-stats">
+        <table>
+          <tbody>
+            <tr>
+              <td>Hit Points</td>
+              <td>{hp}</td>
+            </tr>
+            <tr>
+              <td>Attack</td>
+              <td>{attack}</td>
+            </tr>
+            <tr>
+              <td>Defense</td>
+              <td>{defense}</td>
+            </tr>
+            <tr>
+              <td>Speed</td>
+              <td>{speed}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <ul>{moves}</ul>
+      <button onClick={clicker} className="sp-stats">
+        Check Special Stats
+      </button>
+    </div>
+  );
+};
+
+export default BaseStats;
